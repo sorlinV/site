62b13608215743ff048c5afe199d0e9587db66e4
isidoris <valentin.sorlin@hotmail.com>
2017-04-21 21:23:45 +0200

    repair minijeu.php link

--- minijeu.php
+++ minijeu.php
@@ -7,28 +7,20 @@
     <title>Site Formulaire</title>
 </head>
 <body>
-    
-
-<nav class="patate">
-    <a href="http://localhost/"><input type="button" value="home"></a>
-    <a href="http://localhost/minijeu.php"><input type="button" value="minijeu"></a>
-    <a href="http://localhost/live.php"><input type="button" value="live"></a>
-    <a href="http://localhost/contact.php"><input type="button" value="contact"></a>
-    <a href="http://localhost/cute.php"><input type="button" value="cute"></a>
-</nav>
-<p>Find the password</p>
-<input type="password" id="password" value=""/>
-<input type="button" id="test_pass" onclick="if(document.getElementById('password').value == 123)
-document.getElementById('demo').innerHTML='GG tu as trouvé le mdp';" value="Submit">
-<p id="demo"></p>
-<svg xmlns="http://www.w3.org/2000/svg" width="800" height="800">
-<rect x="250" y="50" width="100" height="100" rx="100" style="fill:orange"></rect>
-<rect x="295" y="100" width="10" height="300" rx="0" style="fill:yellow"></rect>
-<rect x="150" y="200" width="300" height="10" rx="0" style="fill:green"></rect>
-<line x1="300" y1="400" x2="400" y2="500" style="stroke:white"></line>
-<line x1="300" y1="400" x2="200" y2="500" style="stroke:blue"></line>
-
-</svg>
-
-
+    <?php
+        include ('header.html');
+        exec ('cowsay "bonjour je suis une vache"');
+    ?>    
+    <p>Find the password</p>
+    <input type="password" id="password" value=""/>
+    <input type="button" id="test_pass" onclick="if(document.getElementById('password').value == 123)
+    document.getElementById('demo').innerHTML='GG tu as trouvé le mdp';" value="Submit">
+    <p id="demo"></p>
+    <svg xmlns="http://www.w3.org/2000/svg" width="800" height="800">
+        <rect x="250" y="50" width="100" height="100" rx="100" style="fill:orange"></rect>
+        <rect x="295" y="100" width="10" height="300" rx="0" style="fill:yellow"></rect>
+        <rect x="150" y="200" width="300" height="10" rx="0" style="fill:green"></rect>
+        <line x1="300" y1="400" x2="400" y2="500" style="stroke:white"></line>
+        <line x1="300" y1="400" x2="200" y2="500" style="stroke:blue"></line>
+    </svg>
 </body></html>
