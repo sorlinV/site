e0dedc4baf219dbb27f65b823e83ba86f4e9a564
isidoris <valentin.sorlin@hotmail.com>
2017-04-25 09:42:28 +0200

    horoscope

--- index.php
+++ index.php
@@ -10,9 +10,12 @@
 <body>
     <?php
         include ('header.html');
-        exec ('cowsay "bonjour je suis une vache"');
     ?>
     <script>(function(d, s){if(!window.rel){s = d.createElement("script");s.type = "text/javascript";s.async = true;s.id = "radio-de-embedded";s.src = "//www.radio.net/inc/microsite/js/full.js";d.getElementsByTagName("head")[0].appendChild(s);window.rel = true;}}(document));</script><div class="ng-app-embedded"><div ui-view class="microsite embedded-radio-player" data-playertype="web_embedded" data-playerwidth="100%" data-playstation="bloodstream" data-autoplay="true" data-apikey="df04ff67dd3339a6fc19c9b8be164d5b5245ae93"></div></div><noscript><a href="//bloodstream.radio.net" target="_blank">Radio Bloodstream on radio.net</a></noscript>
+    <iframe width="400" height="144" marginheight="0" marginwidth="0" frameborder="0" align="center" src="http://www.astroo.com/horoscope-du-jour.htm" name="astrooh" allowtransparency="true">
+		&lt;!--fin code css perso--&gt;
+		&lt;a href="http://www.astroo.com/horoscope-du-jour.htm" target="_top" title="Cliquez-ici pour afficher l'horoscope quotidien"&gt;&lt;font face="Verdana" size="2"&gt;&lt;b&gt;afficher l'horoscope quotidien&lt;/b&gt;&lt;/font&gt;&lt;/a&gt;
+    </iframe>
     <ul>
         <li class="inline"><a href="#">DELHI</a></li>
         <li class="inline"><a href="#">MUMBAI</a></li>
